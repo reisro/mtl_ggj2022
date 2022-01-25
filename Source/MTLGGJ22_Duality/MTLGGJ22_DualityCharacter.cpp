9d7f1b5d9b9fbe233d5ffeffe33eb66c30e95af2
Rodrigo Reis <rodgamecodebr@gmail.com>
2022-01-25 02:30:34 -0300

    Karlyel Flying Controls tweaked to rotate character and camera

--- Source/MTLGGJ22_Duality/MTLGGJ22_DualityCharacter.cpp
+++ Source/MTLGGJ22_Duality/MTLGGJ22_DualityCharacter.cpp
@@ -18,7 +18,7 @@ AMTLGGJ22_DualityCharacter::AMTLGGJ22_DualityCharacter()
 	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
 
 	// set our turn rates for input
-	BaseTurnRate = 10.f;
+	BaseTurnRate = 20.f;
 	BaseLookUpRate = 45.f;
 
 	// Don't rotate when the controller rotates. Let that just affect the camera.
@@ -28,7 +28,7 @@ AMTLGGJ22_DualityCharacter::AMTLGGJ22_DualityCharacter()
 
 	// Configure character movement
 	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
-	GetCharacterMovement()->RotationRate = FRotator(100.0f, 100.0f, 0.0f); // ...at this rotation rate
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f); // ...at this rotation rate
 	GetCharacterMovement()->JumpZVelocity = 600.f;
 	GetCharacterMovement()->AirControl = 0.2f;
 
@@ -100,14 +100,13 @@ void AMTLGGJ22_DualityCharacter::TouchStopped(ETouchIndex::Type FingerIndex, FVe
 
 void AMTLGGJ22_DualityCharacter::TurnAtRate(float Rate)
 {
-	// calculate delta for this frame from the rate information
-	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
+	
 }
 
 void AMTLGGJ22_DualityCharacter::LookUpAtRate(float Rate)
 {
 	// calculate delta for this frame from the rate information
-	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+	AddControllerPitchInput(-Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
 }
 
 void AMTLGGJ22_DualityCharacter::MoveForward(float Value)
@@ -116,7 +115,7 @@ void AMTLGGJ22_DualityCharacter::MoveForward(float Value)
 	{
 		// find out which way is forward
 		const FRotator Rotation = Controller->GetControlRotation();
-		const FRotator YawRotation(Rotation.Pitch, Rotation.Yaw, 0);
+		const FRotator YawRotation(Rotation.Pitch, 0, 0);
 
 		// get forward vector
 		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
@@ -136,5 +135,8 @@ void AMTLGGJ22_DualityCharacter::MoveRight(float Value)
 		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
 		// add movement in that direction
 		AddMovementInput(Direction, Value);
+
+		// calculate delta for this frame from the rate information
+		AddControllerYawInput(Value * BaseTurnRate * GetWorld()->GetDeltaSeconds());
 	}
 }
